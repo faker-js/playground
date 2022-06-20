2945c6338df7e8fcfeb37b77652e4834904f23d7
Shinigami92 <chrissi92@hotmail.de>
2022-06-20 18:00:34 +0200

    Use force in cypress test

--- playgrounds/nextjs/cypress/e2e/sample.cy.ts
+++ playgrounds/nextjs/cypress/e2e/sample.cy.ts
@@ -1,15 +1,15 @@
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
-describe('Search', () => {
-  it('should search for random word', () => {
+describe("Search", () => {
+  it("should search for random word", () => {
     const word = faker.word.noun();
 
-    cy.visit('https://google.com');
+    cy.visit("https://google.com");
 
-    cy.get('input[name="q"]').type(word);
+    cy.get('input[name="q"]').type(word, { force: true });
 
     cy.get('form[role="search"]').submit();
 
-    cy.url().should('include', `https://www.google.com/search?q=${word}`);
+    cy.url().should("include", `https://www.google.com/search?q=${word}`);
   });
 });
