7135b9f617ea5a1eb6944fa1e3856fafff4d5625
ST-DDT <ST-DDT@gmx.de>
2025-02-10 17:41:22 +0100

    chore: switch from google to bing to avoid bot detection

--- playgrounds/nextjs/cypress/e2e/sample.cy.ts
+++ playgrounds/nextjs/cypress/e2e/sample.cy.ts
@@ -1,15 +1,15 @@
-import { faker } from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
 
-describe("Search", () => {
-  it("should search for random word", () => {
+describe('Search', () => {
+  it('should search for random word', () => {
     const word = faker.word.noun();
 
-    cy.visit("https://google.com");
+    cy.visit('https://www.bing.com');
 
     cy.get('textarea[name="q"]').type(word, { force: true });
 
     cy.get('form[role="search"]').submit();
 
-    cy.url().should("include", `https://www.google.com/search?q=${word}`);
+    cy.url().should('include', `https://www.bing.com/search?q=${word}`);
   });
 });
