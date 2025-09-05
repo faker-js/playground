1a88d67210c9e3fff808f219c8dc5d7f5807dd2b
xDivisionByZerox <leyla.jaehnig@gmx.de>
2025-09-06 01:38:16 +0200

    fix: module resolution in different package managers

--- playgrounds/jest-cjs/jest.config.node20.js
+++ playgrounds/jest-cjs/jest.config.node20.js
@@ -3,7 +3,14 @@
  **/
 module.exports = {
   testEnvironment: 'node',
-  transformIgnorePatterns: ['node_modules\\/(?!(@faker-js\\/faker)\\/).+'],
+  transformIgnorePatterns: [
+    // 'node_modules/(?!@faker-js).+', // required if the projects package manager is "npm"
+    'node_modules/.pnpm/.+/node_modules/(?!@faker-js).+', // required if the package manager is "pnpm"
+    // I have no idea how yarn builds the node_modules directory otherwise the pattern would be here ¯\(o_o)/¯
+    //
+    // NOTE!!!
+    // Exactly one of the patterns must be active at the same time - no more, no less!!!
+  ],
   transform: {
     '^.+\\.(t|j)s$': [
       'ts-jest',
