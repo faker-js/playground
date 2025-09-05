86b45dcded237aa8ab04b7ff2ffcfae5d451f167
xDivisionByZerox <leyla.jaehnig@gmx.de>
2025-09-05 23:16:53 +0200

    feat(playground): jest-cjs

---
+++ playgrounds/jest-cjs/jest.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import("jest").Config}
+ **/
+module.exports = {
+  testEnvironment: 'node',
+  transformIgnorePatterns: ['node_modules\\/(?!(@faker-js\\/faker)\\/).+'],
+  transform: {
+    '^.+\\.(t|j)s$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+  },
+};
